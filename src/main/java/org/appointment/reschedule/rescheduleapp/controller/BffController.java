cd240764f3b40ee60664d8a59ac798f29fa7056a
raghumachani <raghumachani@gmail.com>
2020-06-11 13:37:40 +0530

    added validation for appointment cancellation

--- src/main/java/org/appointment/reschedule/rescheduleapp/controller/BffController.java
+++ src/main/java/org/appointment/reschedule/rescheduleapp/controller/BffController.java
@@ -26,45 +26,45 @@ public class BffController {
 	BffService bffService;
 
 	@RequestMapping(value = "/reschedule", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
-	public Appointment updateAppointmentSlot(@RequestBody Appointment reqPayLoad,@RequestHeader("X-correlationid") String correaltionId) {
+	public Appointment updateAppointmentSlot(@RequestBody Appointment reqPayLoad,
+			@RequestHeader("X-correlationid") String correaltionId) {
 		log.info("updateAppointmentSlot started{}", reqPayLoad.getId());
-		System.out.println("in rest service***************:"+reqPayLoad);
-		
+		System.out.println("in rest service***************:" + reqPayLoad);
+
 		Appointment appt = bffService.findById(reqPayLoad.getId());
-		//:12345
-		//Authorization:pogpoerjgpoom3or34p3vjo4jp
-		if(appt != null) {
+		// :12345
+		// Authorization:pogpoerjgpoom3or34p3vjo4jp
+		if (appt != null) {
 			// Validate token of the request
 			Member members = bffService.findByMemId(appt.getMemberId());
-			
-			if(members.getToken().equals(reqPayLoad.getToken())) {
+
+			if (members.getToken().equals(reqPayLoad.getToken())) {
 				appt.setAppointmentSlot(reqPayLoad.getAppointmentSlot());
-			}else {
+			} else {
 				throw new ResourceNotFoundException("token is not valid", appt.getMemberId());
 			}
-			
-			//
+
+			// set correlation id from the request payload
 			appt.setCorrealtionId(correaltionId);
 		}
 		// Update the timeslot for appointment.
 		bffService.save(appt);
 		return appt;
 	}
-	
+
 	@RequestMapping(value = "/schedule/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
 	public ResponseEntity<Appointment> getAppointmentSlot(@PathVariable(value = "id") int appointmentId) {
 		Appointment appointment = null;
 		try {
 			appointment = bffService.findById(appointmentId);
 		} catch (ResourceNotFoundException e) {
-			throw new ResourceNotFoundException("Appointment Id not found",appointmentId);
+			throw new ResourceNotFoundException("Appointment Id not found", appointmentId);
 		}
 
 		return ResponseEntity.ok(appointment);
 	}
 
-
-	@RequestMapping(value = "/schedule" , produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+	@RequestMapping(value = "/schedule", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
 	public Appointment scheduleApp(@RequestBody Appointment reqPayLoad) {
 		// TODO: Implementation by corresponding team
 		return bffService.findById(reqPayLoad.getId());
@@ -73,19 +73,18 @@ public class BffController {
 	@RequestMapping(value = "/cancel", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
 	public Appointment cancelAppointmentSlot(@RequestBody Appointment reqPayLoad) {
 		log.info("cancelAppointmentSlot started{}", reqPayLoad.getId());
-		System.out.println("cancel in rest service***************:"+reqPayLoad);
+		System.out.println("cancel in rest service***************:" + reqPayLoad);
 		Appointment appt = bffService.findById(reqPayLoad.getId());
-		if(appt != null) {
+		if (appt != null) {
 			// Validate token of the request
 			Member members = bffService.findByMemId(reqPayLoad.getMemberId());
-			
-			if(members.getToken().equals(reqPayLoad.getToken())) {
-				 if(!appt.isCancelled()) {
-				appt.setCancelled(true);
-				 }else {
-					 throw new ResourceNotFoundException("Appointment is already cancelled", reqPayLoad.getMemberId());
-				 }
-			}else {
+			if (members.getToken().equals(reqPayLoad.getToken())) {
+				if (!appt.isCancelled()) {
+					appt.setCancelled(true);
+				} else {
+					throw new ResourceNotFoundException("Appointment is already cancelled", reqPayLoad.getMemberId());
+				}
+			} else {
 				throw new ResourceNotFoundException("token is not valid", appt.getMemberId());
 			}
 		}
